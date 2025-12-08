3841c10686abda3698cc4ff67e7b9cc91c905e2f
Semyon129 <118988799+Semyon129@users.noreply.github.com>
2025-12-08 19:44:08 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import { generatePhotos } from './photo.js';
+import { renderPictures } from './pictures.js';
+import { photos } from './data.js';
+
+renderPictures(photos);
 
 const photos = generatePhotos();
 

eb0753f8aeb951c28ad620a2b15284bfd41c216b
Semyon129 <118988799+Semyon129@users.noreply.github.com>
2025-12-08 21:27:23 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import { generatePhotos } from './photo.js';
-
+import { initBigPicture } from './big-picture.js';
+import { photos } from './data.js';
 const photos = generatePhotos();
-
+renderPictures(photos);
+const thumbnails = document.querySelectorAll('.picture');
+initBigPicture(thumbnails, photos)
 console.log(photos);

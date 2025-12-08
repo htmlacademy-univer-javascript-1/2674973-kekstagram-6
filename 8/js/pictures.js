8109ef3f2108fa451a26450c1d0cf4fb20c513ad
Keks <keks@htmlacademy.ru>
2025-12-08 19:29:09 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/pictures.js
@@ -0,0 +1,29 @@
+import { photos } from './data.js';
+
+const pictureTemplate = document
+  .querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const picturesContainer = document.querySelector('.pictures');
+
+const renderPictures = (data) => {
+  const fragment = document.createDocumentFragment();
+
+  data.forEach(({ url, description, likes, comments }) => {
+    const element = pictureTemplate.cloneNode(true);
+    
+    const img = element.querySelector('.picture__img');
+    img.src = url;
+    img.alt = description;
+
+    element.querySelector('.picture__likes').textContent = likes;
+    element.querySelector('.picture__comments').textContent = comments.length;
+
+    fragment.appendChild(element);
+  });
+
+  picturesContainer.appendChild(fragment);
+};
+
+export { renderPictures };
